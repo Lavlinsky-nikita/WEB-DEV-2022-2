e8da353a6e900ea9efaa2adc72882b684256b035
Lavlinsky-nikita <72300435+Lavlinsky-nikita@users.noreply.github.com>
2022-04-22 19:12:06 +0300

    main

--- Lab2/app/app.py
+++ Lab2/app/app.py
@@ -27,4 +27,43 @@ def cookies():
         response.set_cookie('name', 'qq', expires=0)
     return response
 
+# Добавляем методы, чтобы обрабатывать запросы
+@app.route("/form", methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        context = {}
+        num = []
+        others = []
+        allowed = [' ', '(', ')', '-', '.', '+']
+        
+        for i in request.form['number']:
+            if i.isdigit():
+                num.append(i)
+            else:
+                others.append(i)
+        if len(num) < 10:
+            context = {'error': 'номер слишком короткий'}
+        elif len(others) == 3 and (len(num) == 10):
+            if (others[0]=='.') and (others[1]=='.') and (others[2]=='.'):
+                print('верный формат')
+            else:
+                context = {'error': 'номер содержит недопустимые символы'}
+        elif (int(num[0]) == 8 or (int(num[0])==7 and others[0]=='+')) and len(num) == 11:
+            print('верный формат')
+        else:
+            context = {'error': 'номер слишком длинный'}
+        # Проверка каждого спецсимвола которые у нас попал в thers, на соответсвие допустимых спецсимволов
+        # Если хоть один спец символ не соответсвуте => ошибка
+        for symbol in others:
+            if symbol not in allowed:
+                context = {'error': 'номер содержит недопустимые символы'}
+        
+    
 
+        if context:
+            print(context)
+            return render_template('form.html', context=context, number_format=number_format)
+        else:
+            return request.form
+            
+    return render_template('form.html')
