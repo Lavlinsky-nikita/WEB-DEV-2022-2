f80803ecaa50d6f6e009b3b48d7f68c92010eabf
Lavlinsky-nikita <72300435+Lavlinsky-nikita@users.noreply.github.com>
2022-06-28 16:57:24 +0300

    авторизация

--- Lnv_exam/app/books.py
+++ Lnv_exam/app/books.py
@@ -3,10 +3,22 @@ from models import *
 
 bp = Blueprint('books', __name__, url_prefix='/books')
 
-@bp.route('/new')
+BOOKS_PARAMS = ['name', 'short_description', 'year', 'genries','publishing_house','author', 'page_volume']
+
+def params():
+    return { p: request.form.get(p) for p in BOOKS_PARAMS }
+
+
+@bp.route('/new', methods=['POST'])
 def new():
-    books = Book.query.all()
-    return render_template('books/new.html',books=books)
+    genries = Genre.query.all()
+
+    books = Book(**params())
+    db.session.add(books)
+    db.session.commit()
+    flash(f'Курс {books.name} был успешно добавлен!')
+
+    return render_template('books/new.html', genries=genries)
 
 @bp.route('/edit/<int:book_id>')
 def edit(book_id):
@@ -16,7 +28,9 @@ def edit(book_id):
 def show(book_id):
     return render_template('books/show.html')
 
+# Удаление
 @bp.route('/<int:book_id>/delete', methods=['POST'])
 def delete(book_id):
-    books = Book.query.filter(id=book_id).one()
+    Book.query.filter(id=book_id).delete()
+    db.session.commit()
     return redirect(url_for('index'))
--- Lnv_exam/app/users_policy.py
+++ Lnv_exam/app/users_policy.py
@@ -3,8 +3,8 @@ from flask_login import current_user
 
 
 class UserPolicy:
-    def __init__(self):
-        pass
+    def __init__(self, record=None):
+        self.record = record
 
     def create(self):
         return current_user.is_admin
@@ -14,5 +14,4 @@ class UserPolicy:
     
     def edit(self):
         return current_user.is_admin or current_user.is_moder
-
 
